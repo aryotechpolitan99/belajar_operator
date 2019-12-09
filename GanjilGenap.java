c9d152411d88ca46104d9288e04b6e08a3e3eaed
aryo <aryofreeyoung@gmail.com>
2019-12-09 14:57:02 +0700

    Program test angka ganjil genap

--- GanjilGenap.java
+++ GanjilGenap.java
@@ -2,7 +2,7 @@ public class GanjilGenap{
 	public static void main(String[] args){
 	 int input = Integer.parseInt(args[0]);
 	 String hasil = input %2 == 0 ? "Genap" : "Ganjil";
-	 
+	 System.out.println("Program Test Angka Ganjil Genap");
 	 System.out.println(input + " Ini adalah bilangan " +hasil);
 
 	}
